21d0ae80691c8a6a67117c64b81245a8430a5162
Arpitgavla <164368538+Arpitgavla@users.noreply.github.com>
2024-07-21 13:46:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_HASH = os.environ.get("API_HASH", "1ccf1bd0a86af974e3210a55f662c062")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002222465571"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "6678146480"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "6678146480 , -1002222465571"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
